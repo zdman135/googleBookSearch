03f05c3e8c9b76dc8c3f7339fa2b5a8a994371d8
Rich Jen <rich.rjen@gmail.com>
2019-04-19 18:03:44 -0400

    deploy to heroku
    updated readme
    updated heroku configs

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-mongoose.connect("mongodb://localhost/googlebooks", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true });
 
 
 app.get("/api/books", (req, res) => {
